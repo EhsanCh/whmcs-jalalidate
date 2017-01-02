cb773454aa0027dc7d2d14fb94ff8733765a1fd5
Amin Arjmand <aminarj2000@gmail.com>
2017-01-02 08:42:11 +0330

    Update jalalidate.php

--- includes/hooks/jalalidate.php
+++ includes/hooks/jalalidate.php
@@ -1,10 +1,11 @@
 <?php
 /* *****************************************************
     ** WHMCS Jalali Dates **
-    By Ehsan Chavoshi (HostFa)
+    By Ehsan Chavoshi (HostFa) & Amin Arjmand (https://github.com/sibche2013)
     Date : 2016-04-23
+    Updated : 2017-01-02
     Lincense : MIT License
-    Source : https://github.com/EhsanCh/whmcs-jalalidate
+    Source : https://github.com/EhsanCh/whmcs-jalalidate Or https://github.com/sibche2013/whmcs-jalalidate (WHMCS 7.1.1 Updated)
 ***************************************************** */
 
 add_hook('ClientAreaPage', 1, function($templateVariables) {
@@ -13,43 +14,58 @@ add_hook('ClientAreaPage', 1, function($templateVariables) {
         return;
     $templateVariables['jdate'] = TRUE;
     foreach ($templateVariables['announcements'] as &$announcements) {
-        $announcements['date'] = $announcements['timestamp'] ? jdate("j F", $announcements['timestamp']) : jdate("j F", strtotime($announcements['rawDate']));
+        //$announcements['date'] = $announcements['timestamp'] ? jdate("j F Y", $announcements['timestamp']) : jdate("j F Y", strtotime($announcements['rawDate']));
+		$announcements['rawDate'] = jdate("j F Y", strtotime($announcements['rawDate']));
+		$announcements['timestamp'] = jdate("j F Y", $announcements['timestamp']);
     }
     foreach ($templateVariables['domains'] as &$domains) {
-        $domains['registrationdate'] = jdate("Y/m/d", strtotime($domains['normalisedRegistrationDate']));
-        $domains['nextduedate'] = jdate("Y/m/d", strtotime($domains['normalisedNextDueDate']));
-        $domains['expirydate'] = jdate("Y/m/d", strtotime($domains['normalisedExpiryDate']));
+        $domains['registrationdate'] = '<span class="text-info">'.date("Y/m/d",strtotime($domains['normalisedRegistrationDate'])).'&nbsp;('.jdate("Y/m/d", strtotime($domains['normalisedRegistrationDate'])).')</span>';
+        $domains['nextduedate'] = '<span class="text-danger">'.date("Y/m/d",strtotime($domains['normalisedNextDueDate'])).'&nbsp;('.jdate("Y/m/d", strtotime($domains['normalisedNextDueDate'])).')</span>';
+    }
+	foreach ($templateVariables['renewals'] as &$renewals) {
+        $renewals['expiryDate'] = date("Y/m/d",strtotime($domains['normalisedExpiryDate'])).'&nbsp;('.jdate("Y/m/d", strtotime($domains['normalisedExpiryDate'])).')';
     }
     foreach ($templateVariables['services'] as &$services) {
         $services['regdate'] = jdate("Y/m/d", strtotime($services['normalisedRegDate']));
-        $services['nextduedate'] = jdate("Y/m/d", strtotime($services['normalisedNextDueDate']));
+        $services['nextduedate'] = '<span class="text-danger">'.date("Y/m/d",strtotime($services['normalisedNextDueDate'])).'&nbsp;('.jdate("Y/m/d", strtotime($services['normalisedNextDueDate'])).')</span>';
     }
     foreach ($templateVariables['tickets'] as &$tickets) {
-        $tickets['date'] = jdate("Y/m/d H:i", strtotime($tickets['normalisedDate']));
-        $tickets['lastreply'] = jdate("Y/m/d H:i", strtotime($tickets['normalisedLastReply']));
+        //$tickets['date'] = jdate("Y/m/d H:i", strtotime($tickets['normalisedDate']));
+        $tickets['lastreply'] = '<span class="text-danger">'.jdate("Y/m/d H:i", strtotime($tickets['normalisedLastReply'])).'</span>';
     }
-    foreach ($templateVariables['replies'] as &$replies) {
+/*  foreach ($templateVariables['replies'] as &$replies) {
         $replies['date'] = jdate("Y/m/d H:i", strtotime($replies['date']));
     }
     foreach ($templateVariables['ascreplies'] as &$ascreplies) {
         $ascreplies['date'] = jdate("Y/m/d H:i", strtotime($ascreplies['date']));
-    }
+    }*/
     foreach ($templateVariables['descreplies'] as &$descreplies) {
-        $descreplies['date'] = jdate("Y/m/d H:i", strtotime($descreplies['date']));
+        $descreplies['date'] = '<span class="text-info">'.jdate("Y/m/d H:i", strtotime($descreplies['date'])).'</span>';
     }
     foreach ($templateVariables['invoices'] as &$invoices) {
-        $invoices['datecreated'] = jdate("Y/m/d", strtotime($invoices['normalisedDateCreated']));
-        $invoices['datedue'] = jdate("Y/m/d", strtotime($invoices['normalisedDateDue']));
+        $invoices['datecreated'] = '<span class="text-danger">'.date("Y/m/d", strtotime($invoices['normalisedDateCreated'])).'&nbsp;('.jdate("Y/m/d", strtotime($invoices['normalisedDateCreated'])).')</span>';
+        $invoices['datedue'] = '<span class="text-danger">'.date("Y/m/d", strtotime($invoices['normalisedDateDue'])).'&nbsp;('.jdate("Y/m/d", strtotime($invoices['normalisedDateDue'])).')</span>';
     }
-    
+    foreach ($templateVariables['transactions'] as &$transactions) {
+        $transactions['date'] = '<span class="text-danger">'.date("Y/m/d", strtotime($transactions['date'])).'&nbsp;('.jdate("Y/m/d", strtotime($transactions['date'])).')</span>';
+    }
+    foreach ($templateVariables['emails'] as &$emails) {
+        $emails['date'] = '<span class="text-info">'.date("Y/m/d", strtotime($emails['normalisedDate'])).'&nbsp;('.jdate("Y/m/d", strtotime($emails['normalisedDate'])).')</span>';
+    }
     if ($templateVariables['date']) {
         $templateVariables['jfulldate'] = jdate("l ، j F Y", strtotime($templateVariables['date']));
     }
     
-    $datefields = array('registrationdate','nextduedate','expirydate','regdate','lastupdate','date','duedate','datepaid','datecreated','datedue');
+    $datefields = array('nextduedate','expirydate','lastupdate','date','duedate','datepaid','datecreated','datedue');
+
     foreach ($datefields as $datefield) {
         if ($templateVariables[$datefield])
-            $templateVariables[$datefield] = jdate("Y/m/d", strtotime($templateVariables[$datefield]));
+            $templateVariables[$datefield] = '<span class="text-danger">'.date("Y/m/d",strtotime($templateVariables[$datefield])).'&nbsp;('.jdate("Y/m/d", strtotime($templateVariables[$datefield])).')</span>';
+    }
+	$datefields1 = array('registrationdate','regdate');
+	foreach ($datefields1 as $datefield2) {
+        if ($templateVariables[$datefield2])
+            $templateVariables[$datefield2] = '<span class="text-info">'.date("Y/m/d",strtotime($templateVariables[$datefield2])).'&nbsp;('.jdate("Y/m/d", strtotime($templateVariables[$datefield2])).')</span>';
     }
     
  
