aba7909bfd115c3b56ac038e4a8ac5b82737a84d
Amin Arjmand <aminarj2000@gmail.com>
2017-01-16 08:49:57 +0330

    Update jalalidate.php

--- includes/hooks/jalalidate.php
+++ includes/hooks/jalalidate.php
@@ -1,90 +1,162 @@
 <?php
 /* *****************************************************
-    ** WHMCS Jalali Dates **
-    By Ehsan Chavoshi (HostFa) & Amin Arjmand (https://github.com/sibche2013) & Hamid Ghouli (https://github.com/hamidghouli)
-    Date : 2016-04-23
-    Updated : 2017-01-09
-    Lincense : MIT License
-    Source : https://github.com/EhsanCh/whmcs-jalalidate Or https://github.com/sibche2013/whmcs-jalalidate (WHMCS 7.1.1 Updated)
-***************************************************** */
-add_hook('ClientAreaPage', 1, function ($templateVariables) {
+ ** WHMCS Jalali Dates **
+ By Ehsan Chavoshi (HostFa) & Amin Arjmand (https://github.com/sibche2013) & Hamid Ghouli (https://github.com/hamidghouli)
+ Date : 2016-04-23
+ Updated : 2017-01-16
+ Lincense : MIT License
+ Source : https://github.com/EhsanCh/whmcs-jalalidate Or https://github.com/sibche2013/whmcs-jalalidate (WHMCS 7.1.1 Updated)
+ ***************************************************** */
+add_hook('ClientAreaPage', 1, function($templateVariables)
+{
     // Look for an existing template variable.
-    if (!in_array($templateVariables['language'], array('farsi', 'persian', 'فارسی')))
+    if (!in_array($templateVariables['language'], array(
+        'farsi',
+        'persian',
+        'فارسی'
+    )))
         return;
-    $templateVariables['jdate'] = TRUE;
-    // Announcement Jalali Date
-    foreach ($templateVariables['announcements'] as &$announcements) {
-        $announcements['rawDate'] = jdate("j F Y", strtotime($announcements['rawDate']));
-        $announcements['timestamp'] = jdate("j F Y", $announcements['timestamp']);
-    }
-    // Domains Jalali Date
-    foreach ($templateVariables['domains'] as &$domains) {
-        $domains['registrationdate'] = '<span class="text-info">' . date("Y/m/d", strtotime($domains['normalisedRegistrationDate'])) . '<br>(' . jdate("Y/m/d", strtotime($domains['normalisedRegistrationDate'])) . ')</span>';
-        $domains['nextduedate'] = '<span class="text-danger">' . date("Y/m/d", strtotime($domains['normalisedNextDueDate'])) . '<br>(' . jdate("Y/m/d", strtotime($domains['normalisedNextDueDate'])) . ')</span>';
-    }
-    // Domains Renewal Jalali Date
-    foreach ($templateVariables['renewals'] as &$renewals) {
-        $renewals['expiryDate'] = '<span class="text-danger">' . date("Y/m/d", strtotime($renewals['normalisedExpiryDate'])) . '<br>(' . jdate("Y/m/d", strtotime($renewals['normalisedExpiryDate'])) . ')';
-    }
-    // Quotes Jalali Date
-    foreach ($templateVariables['quotes'] as &$quotes) {
-        $quotes['datecreated'] = '<span class="text-danger">' . date("Y/m/d", strtotime($quotes['normalisedDateCreated'])) . '<br>(' . jdate("Y/m/d", strtotime($quotes['normalisedDateCreated'])) . ')';
-        $quotes['validuntil'] = '<span class="text-danger">' . date("Y/m/d", strtotime($quotes['normalisedValidUntil'])) . '<br>(' . jdate("Y/m/d", strtotime($quotes['normalisedValidUntil'])) . ')';
-    }
-    // Services Jalali Date
-    foreach ($templateVariables['services'] as &$services) {
-        $services['regdate'] = jdate("Y/m/d", strtotime($services['normalisedRegDate']));
-        $services['nextduedate'] = '<span class="text-danger">' . date("Y/m/d", strtotime($services['normalisedNextDueDate'])) . '<br>(' . jdate("Y/m/d", strtotime($services['normalisedNextDueDate'])) . ')</span>';
-    }
-    // Tickets Last Reply Jalali Date
-    foreach ($templateVariables['tickets'] as &$tickets) {
-        $tickets['lastreply'] = '<span class="text-danger">' . jdate("Y/m/d H:i", strtotime($tickets['normalisedLastReply'])) . '</span>';
-    }
-    // Tickets Reply Jalali Date
-    foreach ($templateVariables['descreplies'] as &$descreplies) {
-        $descreplies['date'] = '<span class="text-info">' . jdate("Y/m/d H:i", strtotime($descreplies['date'])) . '</span>';
-    }
-    // Invoices Jalali Date
+    //$templateVariables['jdate'] = TRUE;
+    
+    // Invoices Jalali Date - clientarea.php?action=invoices
     foreach ($templateVariables['invoices'] as &$invoices) {
-        $invoices['datecreated'] = '<span class="text-danger">' . date("Y/m/d", strtotime($invoices['normalisedDateCreated'])) . '<br>(' . jdate("Y/m/d", strtotime($invoices['normalisedDateCreated'])) . ')</span>';
-        $invoices['datedue'] = '<span class="text-danger">' . date("Y/m/d", strtotime($invoices['normalisedDateDue'])) . '<br>(' . jdate("Y/m/d", strtotime($invoices['normalisedDateDue'])) . ')</span>';
+        if (strtotime($invoices['datecreated']) > 0)
+            $invoices['datecreated'] = '<span class="text-info">' . date("Y/m/d", strtotime($invoices['normalisedDateCreated'])) . '<br>' . jdate("Y/m/d", strtotime($invoices['normalisedDateCreated'])) . '</span>';
+        if (strtotime($invoices['datedue']) > 0)
+            $invoices['datedue'] = '<span class="text-danger">' . date("Y/m/d", strtotime($invoices['normalisedDateDue'])) . '<br>' . jdate("Y/m/d", strtotime($invoices['normalisedDateDue'])) . '</span>';
     }
-    // Transactions Jalali Date
+    
+    // Quotes Jalali Date - clientarea.php?action=quotes
+    foreach ($templateVariables['quotes'] as &$quotes) {
+        if (strtotime($quotes['datecreated']) > 0)
+            $quotes['datecreated'] = '<span class="text-info">' . date("Y/m/d", strtotime($quotes['normalisedDateCreated'])) . '<br>' . jdate("Y/m/d", strtotime($quotes['normalisedDateCreated'])) . '</span>';
+        if (strtotime($quotes['validuntil']) > 0)
+            $quotes['validuntil'] = '<span class="text-danger">' . date("Y/m/d", strtotime($quotes['normalisedValidUntil'])) . '<br>' . jdate("Y/m/d", strtotime($quotes['normalisedValidUntil'])) . '</span>';
+    }
+    
+    // Domains Jalali Date - clientarea.php?action=domains
+    foreach ($templateVariables['domains'] as &$domains) {
+        if (strtotime($domains['registrationdate']) > 0)
+            $domains['registrationdate'] = '<span class="text-info">' . date("Y/m/d", strtotime($domains['normalisedRegistrationDate'])) . '<br>' . jdate("Y/m/d", strtotime($domains['normalisedRegistrationDate'])) . '</span>';
+        if (strtotime($domains['nextduedate']) > 0)
+            $domains['nextduedate'] = '<span class="text-danger">' . date("Y/m/d", strtotime($domains['normalisedNextDueDate'])) . '<br>' . jdate("Y/m/d", strtotime($domains['normalisedNextDueDate'])) . '</span>';
+    }
+    
+    // Domains Renewal Jalali Date - clientarea.php?action=domains#tabRenew
+    foreach ($templateVariables['renewals'] as &$renewals) {
+        if (strtotime($renewals['expiryDate']) > 0)
+            $renewals['expiryDate'] = '<span class="text-danger">' . date("Y/m/d", strtotime($renewals['normalisedExpiryDate'])) . '<br>' . jdate("Y/m/d", strtotime($renewals['normalisedExpiryDate'])) . '</span>';
+    }
+    
+    // Services Jalali Date - clientarea.php?action=services
+    foreach ($templateVariables['services'] as &$services) {
+        if (strtotime($services['regdate']) > 0)
+            $services['regdate'] = '<span class="text-danger">' . date("Y/m/d", strtotime($services['normalisedRegDate'])) . '<br>' . jdate("Y/m/d", strtotime($services['normalisedRegDate'])) . '</span>';
+        if (strtotime($services['nextduedate']) > 0)
+            $services['nextduedate'] = '<span class="text-danger">' . date("Y/m/d", strtotime($services['normalisedNextDueDate'])) . '<br>' . jdate("Y/m/d", strtotime($services['normalisedNextDueDate'])) . '</span>';
+    }
+    
+    // Tickets Last Reply Jalali Date - supporttickets.php
+    foreach ($templateVariables['tickets'] as &$tickets) {
+        if (strtotime($tickets['lastreply']) > 0)
+            $tickets['lastreply'] = '<span class="text-info">' . date("Y/m/d H:i", strtotime($tickets['normalisedLastReply'])) . '<br>' . jdate("Y/m/d H:i", strtotime($tickets['normalisedLastReply'])) . '</span>';
+    }
+    
+    // Tickets Reply Jalali Date - viewticket.php?tid=xxx
+    foreach ($templateVariables['descreplies'] as &$descreplies) {
+        if (strtotime($descreplies['date']) > 0)
+            $descreplies['date'] = '<span class="text-info">' . date("Y/m/d H:i", strtotime($descreplies['date'])) . '<br>' . jdate("Y/m/d H:i", strtotime($descreplies['date'])) . '</span>';
+    }
+    
+    // Emails History Jalali Date - clientarea.php?action=emails
+    foreach ($templateVariables['emails'] as &$emails) {
+        if (strtotime($emails['date']) > 0)
+            $emails['date'] = '<span class="text-info">' . date("Y/m/d H:i", strtotime($emails['normalisedDate'])) . '<br>' . jdate("Y/m/d H:i", strtotime($emails['normalisedDate'])) . '</span>';
+    }
+    
+    // Announcement Jalali Date - announcements.php
+    foreach ($templateVariables['announcements'] as &$announcements) {
+        $announcements['rawDate']   = jdate("j F Y", strtotime($announcements['rawDate']));
+        $announcements['timestamp'] = jdate("j F Y", $announcements['timestamp']);
+        
+    }
+    
+    /*// Transactions Jalali Date
     foreach ($templateVariables['transactions'] as &$transactions) {
         $transactions['date'] = '<span class="text-danger">' . date("Y/m/d", strtotime($transactions['date'])) . '<br>(' . jdate("Y/m/d", strtotime($transactions['date'])) . ')</span>';
     }
-    // Emails History Jalali Date
-    foreach ($templateVariables['emails'] as &$emails) {
-        $emails['date'] = '<span class="text-info">' . date("Y/m/d", strtotime($emails['normalisedDate'])) . '<br>(' . jdate("Y/m/d", strtotime($emails['normalisedDate'])) . ')</span>';
-    }
+        
     if ($templateVariables['date']) {
         $templateVariables['jfulldate'] = jdate("l ، j F Y", strtotime($templateVariables['date']));
-    }
-
-    $datefields = array( 'expirydate','nextduedate', 'date', 'duedate', 'datepaid', 'datedue');
+    }*/
+    
+    $datefields = array(
+        'expirydate',
+        'duedate',
+        'datepaid',
+        'datedue',
+        'nextduedate',
+        'validuntil'
+    );
     foreach ($datefields as $datefield) {
-        if ($templateVariables[$datefield])
-            $templateVariables[$datefield] = '<span class="text-danger">' . date("Y/m/d", strtotime($templateVariables[$datefield])) . '<br>(' . jdate("Y/m/d", strtotime($templateVariables[$datefield])) . ')</span>';
+        if (strtotime($templateVariables[$datefield]) > 0)
+            $templateVariables[$datefield] = '<span class="text-danger">' . date("Y/m/d", strtotime($templateVariables[$datefield])) . '&nbsp;(' . jdate("Y/m/d", strtotime($templateVariables[$datefield])) . ')</span>';
     }
-    $datefields1 = array('registrationdate', 'datecreated');
+    
+    $datefields1 = array(
+        'registrationdate',
+        'datecreated',
+        'regdate',
+        'date'
+    );
     foreach ($datefields1 as $datefield2) {
-        if ($templateVariables[$datefield2])
-            $templateVariables[$datefield2] = '<span class="text-info">' . date("Y/m/d", strtotime($templateVariables[$datefield2])) . '<br>(' . jdate("Y/m/d", strtotime($templateVariables[$datefield2])) . ')</span>';
+        if (strtotime($templateVariables[$datefield2]) > 0)
+            $templateVariables[$datefield2] = '<span class="text-info">' . date("Y/m/d", strtotime($templateVariables[$datefield2])) . '&nbsp;(' . jdate("Y/m/d", strtotime($templateVariables[$datefield2])) . ')</span>';
     }
+    
+    //echo "<pre>";
+    //print_r($templateVariables);
+    //echo "</pre>";
     return $templateVariables;
 });
 // Product Details Jalali Date
-add_hook('ClientAreaProductDetailsPreModuleTemplate', 1, function ($templateVariables) {
-    $templateVariables['jdate'] = TRUE;
-    $datefields = array('nextduedate');
+add_hook('ClientAreaProductDetailsPreModuleTemplate', 1, function($templateVariables)
+{
+    
+    if (!in_array($templateVariables[clientsdetails][language], array(
+        'farsi',
+        'persian',
+        'فارسی'
+    )))
+        return;
+    
+    //$templateVariables['jdate'] = TRUE;
+    $datefields = array(
+        'nextduedate'
+    );
     foreach ($datefields as $datefield) {
-        if ($templateVariables[$datefield])
-            $templateVariables[$datefield] = '<span class="text-danger">' . date("Y/m/d", strtotime($templateVariables[$datefield])) . '<br>(' . jdate("Y/m/d", strtotime($templateVariables[$datefield])) . ')</span>';
+        if (strtotime($templateVariables[$datefield]) > 0)
+            $templateVariables[$datefield] = '<span class="text-danger">' . date("Y/m/d", strtotime($templateVariables[$datefield])) . '&nbsp;(' . jdate("Y/m/d", strtotime($templateVariables[$datefield])) . ')</span>';
     }
-    $datefields1 = array('regdate', 'lastupdate');
+    
+    $datefields1 = array(
+        'regdate'
+    );
     foreach ($datefields1 as $datefield2) {
-        if ($templateVariables[$datefield2])
-            $templateVariables[$datefield2] = '<span class="text-info">' . date("Y/m/d", strtotime($templateVariables[$datefield2])) . '<br>(' . jdate("Y/m/d", strtotime($templateVariables[$datefield2])) . ')</span>';
+        if (strtotime($templateVariables[$datefield2]) > 0)
+            $templateVariables[$datefield2] = '<span class="text-info">' . date("Y/m/d", strtotime($templateVariables[$datefield2])) . '&nbsp;(' . jdate("Y/m/d", strtotime($templateVariables[$datefield2])) . ')</span>';
     }
+    
+    $datefields2 = array(
+        'lastupdate'
+    );
+    foreach ($datefields2 as $datefield3) {
+        if (strtotime($templateVariables[$datefield3]) > 0)
+            $templateVariables[$datefield3] = '<span class="text-info">' . date("Y/m/d H:i", strtotime($templateVariables[$datefield3])) . '&nbsp;(' . jdate("Y/m/d H:i", strtotime($templateVariables[$datefield3])) . ')</span>';
+    }
+    
+    //echo "<pre>";
+    //print_r($templateVariables);
+    //echo "</pre>";
     return $templateVariables;
 });
